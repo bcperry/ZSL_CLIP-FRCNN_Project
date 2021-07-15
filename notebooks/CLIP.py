3900887e6440e41188fc580c234a0d502e204ddd
bcperry <blainecperry@gmail.com>
2021-07-15 13:52:05 +0000

    Added CLIP.py and changed CLIP.ipynb to work with Azure

---
+++ notebooks/CLIP.py
@@ -0,0 +1,312 @@
+import os
+import collections
+import json
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import tensorflow_hub as hub
+import tensorflow_text as text
+import tensorflow_addons as tfa
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from tqdm import tqdm
+from keras.callbacks import Callback
+from azureml.core import Run
+
+
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data-folder', type=str, dest='data_folder', default='data', help='data folder containing tfrecord files')
+parser.add_argument('--batch-size', type=int, dest='batch_size', default=256, help='mini batch size for training')
+parser.add_argument('--num-epochs', type=int, dest='num_epochs', default=50, help='number of epochs for training')
+parser.add_argument('--num-train-examples', type=int, dest='num_train_examples', default=50, help='number of training examples')
+parser.add_argument('--num-validation-examples', type=int, dest='num_validation_examples', default=50, help='number of validation examples')
+
+args = parser.parse_args()
+
+num_epochs = args.num_epochs
+batch_size = args.batch_size
+tfrecords_dir = args.data_folder
+train_example_count = args.num_train_examples
+valid_example_count = args.num_validation_examples
+
+print('training dataset is stored here:', tfrecords_dir)
+
+feature_description = {
+    "caption": tf.io.FixedLenFeature([], tf.string),
+    "raw_image": tf.io.FixedLenFeature([], tf.string),
+}
+
+def read_example(example):
+    features = tf.io.parse_single_example(example, feature_description)
+    raw_image = features.pop("raw_image")
+    features["image"] = tf.image.resize(
+        tf.image.decode_jpeg(raw_image, channels=3), size=(299, 299)
+    )
+    return features
+
+
+def get_dataset(file_pattern, batch_size):
+
+    return (
+        tf.data.TFRecordDataset(tf.data.Dataset.list_files(file_pattern))
+        .map(
+            read_example,
+            num_parallel_calls=tf.data.experimental.AUTOTUNE,
+            deterministic=False,
+        )
+        .shuffle(batch_size * 10)
+        .prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+        .batch(batch_size)
+    )
+
+
+"""
+## Implement the projection head
+The projection head is used to transform the image and the text embeddings to
+the same embedding space with the same dimensionality.
+"""
+
+def project_embeddings(
+    embeddings, num_projection_layers, projection_dims, dropout_rate
+):
+    projected_embeddings = layers.Dense(units=projection_dims)(embeddings)
+    for _ in range(num_projection_layers):
+        x = tf.nn.gelu(projected_embeddings)
+        x = layers.Dense(projection_dims)(x)
+        x = layers.Dropout(dropout_rate)(x)
+        x = layers.Add()([projected_embeddings, x])
+        projected_embeddings = layers.LayerNormalization()(x)
+    return projected_embeddings
+
+
+"""
+## Implement the vision encoder
+In this example, we use Resnet50V2
+from [Keras Applications](https://keras.io/api/applications/) as the base for the
+vision encoder.
+"""
+
+def create_vision_encoder(
+    num_projection_layers, projection_dims, dropout_rate, trainable=False
+):
+    # Load the pre-trained resnet model to be used as the base encoder.
+    ResNet = keras.applications.ResNet50V2(
+        include_top=False, weights="imagenet", pooling="avg"
+    )
+    # Set the trainability of the base encoder.
+    for layer in ResNet.layers:
+        layer.trainable = trainable
+    # Receive the images as inputs.
+    inputs = layers.Input(shape=(299, 299, 3), name="image_input")
+    # Preprocess the input image.
+    ResNet_input = tf.keras.applications.resnet_v2.preprocess_input(inputs)
+    # Generate the embeddings for the images using the resnet model.
+    embeddings = ResNet(ResNet_input)
+    # Project the embeddings produced by the model.
+    outputs = project_embeddings(
+        embeddings, num_projection_layers, projection_dims, dropout_rate
+    )
+    # Create the vision encoder model.
+    return keras.Model(inputs, outputs, name="vision_encoder")
+
+
+"""
+## Implement the text encoder
+We use [BERT](https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-12_H-256_A-4/1)
+from [TensorFlow Hub](https://tfhub.dev) as the text encoder
+"""
+
+def create_text_encoder(
+    num_projection_layers, projection_dims, dropout_rate, trainable=False
+):
+    # Load the BERT preprocessing module.
+    preprocess = hub.KerasLayer(
+        "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/2",
+        name="text_preprocessing",
+    )
+    # Load the pre-trained BERT model to be used as the base encoder.
+    bert = hub.KerasLayer(
+        "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1",
+        "bert",
+    )
+    # Set the trainability of the base encoder.
+    bert.trainable = trainable
+    # Receive the text as inputs.
+    inputs = layers.Input(shape=(), dtype=tf.string, name="text_input")
+    # Preprocess the text.
+    bert_inputs = preprocess(inputs)
+    # Generate embeddings for the preprocessed text using the BERT model.
+    embeddings = bert(bert_inputs)["pooled_output"]
+    # Project the embeddings produced by the model.
+    outputs = project_embeddings(
+        embeddings, num_projection_layers, projection_dims, dropout_rate
+    )
+    # Create the text encoder model.
+    return keras.Model(inputs, outputs, name="text_encoder")
+
+
+
+
+"""
+## Implement the dual encoder
+To calculate the loss, we compute the pairwise dot-product similarity between
+each `caption_i` and `images_j` in the batch as the predictions.
+The target similarity between `caption_i`  and `image_j` is computed as
+the average of the (dot-product similarity between `caption_i` and `caption_j`)
+and (the dot-product similarity between `image_i` and `image_j`).
+Then, we use crossentropy to compute the loss between the targets and the predictions.
+"""
+
+class DualEncoder(keras.Model):
+    def __init__(self, text_encoder, image_encoder, temperature=1.0, **kwargs):
+        super(DualEncoder, self).__init__(**kwargs)
+        self.text_encoder = text_encoder
+        self.image_encoder = image_encoder
+        self.temperature = temperature
+        self.loss_tracker = keras.metrics.Mean(name="loss")
+
+    @property
+    def metrics(self):
+        return [self.loss_tracker]
+
+    def call(self, features, training=False):
+        # Place each encoder on a separate GPU (if available).
+        # TF will fallback on available devices if there are fewer than 2 GPUs.
+        
+        with tf.device("/gpu:0"):
+            # Get the embeddings for the captions.
+            caption_embeddings = text_encoder(features["caption"], training=training)
+        with tf.device("/gpu:1"):
+            # Get the embeddings for the images.
+            image_embeddings = vision_encoder(features["image"], training=training)
+          
+        return caption_embeddings, image_embeddings
+
+    def compute_loss(self, caption_embeddings, image_embeddings):
+        # logits[i][j] is the dot_similarity(caption_i, image_j).
+        logits = (
+            tf.matmul(caption_embeddings, image_embeddings, transpose_b=True)
+            / self.temperature
+        )
+        # images_similarity[i][j] is the dot_similarity(image_i, image_j).
+        images_similarity = tf.matmul(
+            image_embeddings, image_embeddings, transpose_b=True
+        )
+        # captions_similarity[i][j] is the dot_similarity(caption_i, caption_j).
+        captions_similarity = tf.matmul(
+            caption_embeddings, caption_embeddings, transpose_b=True
+        )
+        # targets[i][j] = avarage dot_similarity(caption_i, caption_j) and dot_similarity(image_i, image_j).
+        targets = keras.activations.softmax(
+            (captions_similarity + images_similarity) / (2 * self.temperature)
+        )
+        # Compute the loss for the captions using crossentropy
+        captions_loss = keras.losses.categorical_crossentropy(
+            y_true=targets, y_pred=logits, from_logits=True
+        )
+        # Compute the loss for the images using crossentropy
+        images_loss = keras.losses.categorical_crossentropy(
+            y_true=tf.transpose(targets), y_pred=tf.transpose(logits), from_logits=True
+        )
+        # Return the mean of the loss over the batch.
+        return (captions_loss + images_loss) / 2
+
+    def train_step(self, features):
+        with tf.GradientTape() as tape:
+            # Forward pass
+            caption_embeddings, image_embeddings = self(features, training=True)
+            loss = self.compute_loss(caption_embeddings, image_embeddings)
+        # Backward pass
+        gradients = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+        # Monitor loss
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
+
+    def test_step(self, features):
+        caption_embeddings, image_embeddings = self(features, training=False)
+        loss = self.compute_loss(caption_embeddings, image_embeddings)
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
+
+
+"""
+## Train the dual encoder model
+In this experiment, we freeze the base encoders for text and images, and make only
+the projection head trainable.
+"""
+
+vision_encoder = create_vision_encoder(
+    num_projection_layers=1, projection_dims=256, dropout_rate=0.1
+)
+text_encoder = create_text_encoder(
+    num_projection_layers=1, projection_dims=256, dropout_rate=0.1
+)
+dual_encoder = DualEncoder(text_encoder, vision_encoder, temperature=0.05)
+dual_encoder.compile(
+    optimizer=tfa.optimizers.AdamW(learning_rate=0.001, weight_decay=0.001), metrics=['accuracy']
+)
+
+"""
+Note that training the model with 60,000 image-caption pairs, with a batch size of 256,
+takes around 12 minutes per epoch using a V100 GPU accelerator. If 2 GPUs are available,
+the epoch takes around 8 minutes.
+"""
+
+print(f"Number of GPUs: {len(tf.config.list_physical_devices('GPU'))}")
+print(f"Number of examples (caption-image pairs): {train_example_count}")
+print(f"Batch size: {batch_size}")
+print(f"Steps per epoch: {int(np.ceil(train_example_count / batch_size))}")
+train_dataset = get_dataset(os.path.join(tfrecords_dir, "train-*.tfrecord"), batch_size)
+valid_dataset = get_dataset(os.path.join(tfrecords_dir, "valid-*.tfrecord"), batch_size)
+
+
+
+# Create a learning rate scheduler callback.
+reduce_lr = keras.callbacks.ReduceLROnPlateau(
+    monitor="val_loss", factor=0.2, patience=3
+)
+# Create an early stopping callback.
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor="val_loss", patience=5, restore_best_weights=True
+)
+
+# start an Azure ML run
+run = Run.get_context()
+
+
+class LogRunMetrics(Callback):
+    # callback at the end of every epoch
+    def on_epoch_end(self, epoch, log):
+        # log a value repeated which creates a list
+        run.log('Loss', log['val_loss'])
+
+history = dual_encoder.fit(
+    train_dataset,
+    epochs=num_epochs,
+    validation_data=valid_dataset,
+    callbacks=[reduce_lr, early_stopping, LogRunMetrics()],
+)
+
+
+# create a ./outputs/model folder in the compute target
+# files saved in the "./outputs" folder are automatically uploaded into run history
+os.makedirs('./outputs/model', exist_ok=True)
+
+
+# save model weights
+print("Training completed. Saving vision and text encoders...")
+vision_encoder.save("./outputs/model/vision_encoder")
+text_encoder.save("./outputs/model/text_encoder")
+print("model saved in ./outputs/model folder")
+
+
+
+
+
+
+    
